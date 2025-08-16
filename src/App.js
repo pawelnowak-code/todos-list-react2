ac2ab0fbd652c247fb598925f2b057bafdb2fa60
Paweł Nowak <hitec00@interia.pl>
2025-08-16 11:48:03 +0200

    simple build test

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const tasks = [
   { id: 1, content: "zjesc obiad", done: false },
   { id: 2, content: "nauczy sie react-a", done: false },
   { id: 3, content: "nauczy sie pythona", done: true },
+  { id: 4, content: "nauczy sie JS", done: true },
 ];
 
 const hideDoneTasks = false;
@@ -17,17 +18,17 @@ const hideDoneTasks = false;
 function App() {
   return (
     <Container>
-      <Header 
-        title="Lista zadań 2" 
-      /> 
-      <Section 
-        title="Dodaj nowe zadanie" 
-        body={<Form />} 
+      <Header
+        title="Lista zadań 2"
       />
-      <Section 
-        title="Lista zadań" 
-        extraHeaderContent={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />} 
-        body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />} 
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form />}
+      />
+      <Section
+        title="Lista zadań"
+        extraHeaderContent={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />}
+        body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
       />
     </Container>
   );
